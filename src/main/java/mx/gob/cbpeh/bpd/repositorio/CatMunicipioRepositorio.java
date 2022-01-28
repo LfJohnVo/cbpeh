cd49221c1cc67fe9cc46765e5d886ffa4f4789ed
Uriel Santiago Reyes Paredes <uriel.santiago@silent4business.com>
2022-01-28 11:13:31 -0600

    Revision al consumo de api para concentrado

--- src/main/java/mx/gob/cbpeh/bpd/repositorio/CatMunicipioRepositorio.java
+++ src/main/java/mx/gob/cbpeh/bpd/repositorio/CatMunicipioRepositorio.java
@@ -3,13 +3,15 @@ package mx.gob.cbpeh.bpd.repositorio;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import mx.gob.cbpeh.bpd.modelo.*;
 
 @Repository("municipioRepositorio")
-public interface CatMunicipioRepositorio extends JpaRepository<CatMunicipio, Integer>{
-	
-	List<CatMunicipio> findByCodigoEstadoOrderByMunicipioDetalleAsc(String codigoEstado);
-	
+public interface CatMunicipioRepositorio extends JpaRepository<CatMunicipio, Integer> {
 
+	List<CatMunicipio> findByCodigoEstadoOrderByMunicipioDetalleAsc(String codigoEstado);
+
+	@Query(value = "SELECT * FROM cat_municipio WHERE codigo_estado = ?1 AND codigo_municipio = ?2", nativeQuery = true)
+	CatMunicipio findByCodigoEstadoAndCodigoMunicipio(String codigoEstado, String codigoMunicipio);
 }
